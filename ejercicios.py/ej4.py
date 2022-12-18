02b444ee9da9f49b149f6aa5099de5f2d6e94710
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-12-18 10:13:10 +0000

    c

--- ejercicios.py/ej4.py
+++ ejercicios.py/ej4.py
@@ -4,4 +4,18 @@ def rotar(mat):
   n= len(mat)
   for i in range(n):
     for j in range(i):
-      temp= mat
+      temp= mat[i][j]
+      mat[i][j]= mat[j][i]
+      mat[j][i]= temp
+    for i in range(n):
+      for j in range(n//2):
+        temp= mat[i][j]
+        mat[i][j]= mat[j][n-j-1]
+        mat[i][n-j-1]= temp
+
+if __name__='__main__':
+  mat= [[7, 4, 1],
+        [8, 5, 2],
+        [9, 6, 3]]
+  rotate(mat)
+  F
